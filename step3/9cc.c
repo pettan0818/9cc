5905a740e38ee1d8776d5a2a6348c6e2aaa81ca6
pettan0818 <pettan0818+git@gmail.com>
2018-11-07 23:07:38 +0000

    [maintain] make tab to spacex4

--- step3/9cc.c
+++ step3/9cc.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
-		fprintf(stderr, "Invalid num of args.\n");
-		return 1;
-	}
+    if (argc != 2) {
+        fprintf(stderr, "Invalid num of args.\n");
+        return 1;
+    }
 
     char *p = argv[1];
 
 
-	printf(".intel_syntax noprefix\n");
-	printf(".global main\n");
-	printf("main:\n");
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
     // アドレス参照子(&)
-	printf("  mov rax, %ld\n", strtol(p, &p, 10));  // strtolは、(対象文字列, 変換できなかった残りの文字列を格納するアドレス, 基数)なので、10+4なら、10が戻り値、+4がポインタ先に格納となる。
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));  // strtolは、(対象文字列, 変換できなかった残りの文字列を格納するアドレス, 基数)なので、10+4なら、10が戻り値、+4がポインタ先に格納となる。
 
     // while(var) でvarが0でない場合は、True.
     while (*p) {
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-	printf("  ret\n");
-	return 0;
+    printf("  ret\n");
+    return 0;
 }
 
